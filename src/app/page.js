81e1dadb0f030c600497b07a623b4b3b7bc76548
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-11-08 13:22:33 +0500

    create section 4

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import HeaderSection from './sections/HeaderSection';
 import Section2 from './sections/Section2';
 import Section3 from './sections/Section3';
+import Section4 from './sections/Section4';
 
 export default function Home() {
   return (
@@ -16,17 +17,7 @@ export default function Home() {
       <Section3 />
 
       {/* FOURTH SECTION */}
-      <section className="w-full min-h-screen flex items-center justify-center relative">
-        <Image src="/background.png" width={2000} height={150} alt="ZIMA Pro Logo" />
-
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 flex flex-col items-center">
-          <div className="w-4 h-6 border-2 border-white rounded-full flex items-start">
-            <div className="w-1 h-2 bg-white rounded-full animate-bounce mb-1"></div>
-          </div>
-        </div>
-      </section>
-
+      <Section4 />
 
       {/* FIFTH SECTION */}
       <section>
---
+++ src/app/sections/Section4.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+export default function Section4() {
+  return (
+    <section className="w-full min-h-screen flex items-center justify-center relative">
+        <Image src="/background.png" width={2000} height={150} alt="ZIMA Pro Logo" />
+
+        {/* Scroll Indicator */}
+        <div className="absolute bottom-8 flex flex-col items-center">
+          <div className="w-4 h-6 border-2 border-white rounded-full flex items-start">
+            <div className="w-1 h-2 bg-white rounded-full animate-bounce mb-1"></div>
+          </div>
+        </div>
+      </section>
+  )
+}
